7e3aa04545e4c3f1bec0528121047ea1264eb8fc
saranjiiva <saranjiiva@gmail.com>
2026-02-08 12:32:03 +0530

    Create viewer.js

---
+++ js/viewer.js
@@ -0,0 +1,30 @@
+protect("viewer");
+
+const table = document.getElementById("table");
+
+function render() {
+  const data = getData();
+  table.innerHTML = `
+    <tr><th>ID</th><th>Name</th><th>Diagnosis</th><th>Owner</th></tr>
+    ${data.map(r => `
+      <tr>
+        <td>${r.id}</td>
+        <td>${r.name}</td>
+        <td>${r.diagnosis}</td>
+        <td>${r.owner}</td>
+      </tr>`).join("")}
+  `;
+}
+
+function logout() {
+  localStorage.clear();
+  window.location.href = "index.html";
+}
+
+function protect(role) {
+  if (localStorage.getItem("role") !== role) {
+    window.location.href = "index.html";
+  }
+}
+
+render();
