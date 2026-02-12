a0120c0913b8e117b58efa59db7f80b739746828
saranjiiva <saranjiiva@gmail.com>
2026-02-12 19:55:35 +0530

    Create fire base.js

---
+++ js/fire base.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBJOFL_iqY3I4Otu7bxR0l3zFpCC-Z091I",
+    authDomain: "college-attendance-syste-c9ffd.firebaseapp.com",
+    projectId: "college-attendance-syste-c9ffd",
+    storageBucket: "college-attendance-syste-c9ffd.firebasestorage.app",
+    messagingSenderId: "471403216848",
+    appId: "1:471403216848:web:d79f65dc12f9e439b0c3d5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
